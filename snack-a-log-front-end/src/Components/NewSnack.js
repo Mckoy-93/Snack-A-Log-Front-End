caa7b0de57f1fd731852b7cfdd0ef252e8700454
SangUn Park <Sangunpark1@gmail.com>
2023-05-15 11:52:43 -0400

    trying to solve netlify issues

--- snack-a-log-front-end/src/Components/NewSnack.js
+++ snack-a-log-front-end/src/Components/NewSnack.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Snack from "./Snack";
 
 
 export default function NewSnack() {
