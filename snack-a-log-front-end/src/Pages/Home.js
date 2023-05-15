7b10b13ce0c6b5a907245b6588e228caad8372af
SangUn Park <Sangunpark1@gmail.com>
2023-05-14 20:52:31 -0400

    adding calorie conversion

--- snack-a-log-front-end/src/Pages/Home.js
+++ snack-a-log-front-end/src/Pages/Home.js
@@ -3,6 +3,7 @@ function Home() {
       <div>
         <h2>Welcome</h2>
         <h3>Snack-Log App created by SangUn Park and Markeya Mckoy-Carree for Pursuit Cohort 9.3</h3>
+        <h4>Calorie Conversions are done based on an article on verywellfit.com, these are very vague estimations.</h4>
       </div>
     );
   }
