4c978835914eacc2f2ca56c12f711b5a34351068
SangUn Park <Sangunpark1@gmail.com>
2023-05-15 10:57:33 -0400

    minor qol things

--- snack-a-log-front-end/src/Components/NavBar.js
+++ snack-a-log-front-end/src/Components/NavBar.js
@@ -1,7 +1,29 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Nav = () => {
+  const [snacks, setSnacks] = useState([]);
+  let calories = 0
+
+  useEffect(() => {
+    const API = process.env.REACT_APP_API_URL;
+    axios
+      .get(`${API}/snacks`)
+      .then((response) => {
+        setSnacks(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  for (let i = 0; i < snacks.length; i++){
+    calories += snacks[i].calories
+  }
+
+
   return (
     <div className="nav" id="nav">
       <h2>Snack-A-Log!</h2>
@@ -16,6 +38,7 @@ const Nav = () => {
       <Link className="nav-link" to="/snacks/new">
         New Snack
       </Link>
+      <strong>Cal Counter: {calories}</strong>
       <br></br>
     </div>
   );
--- snack-a-log-front-end/src/Components/Snack.js
+++ snack-a-log-front-end/src/Components/Snack.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Snack( { snack }) {
     return (
         <div key={snack.id}>
-            <h2>Name: {snack.name}</h2>
+            <h2 style={{color: snack.added_sugars ? "Red" : "Green"}}>{snack.name}</h2>
             <h3>Calorie: {snack.calories} kcal</h3>
             {snack.added_sugars ? <h3>Added Sugar: True</h3> : <h3>Added Sugar: False</h3>}
             <Link to={`/snacks/${snack.id}`}>Click Here to See More Info❓</Link>
